2f114f7f51fe093716ca1f772cd81f37b7c656fc
CHENYANGLu <clu3842@gmail.com>
2020-02-13 00:33:15 +0800

    feat: New feature: handle the response
    return promise when request state changes, convert the response data and header to JSON format

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,38 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
-import {buildUrl} from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { buildUrl } from './helpers/url'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
-function axios(config: AxiosRequestConfig):void {
+
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
-function processConfig(config: AxiosRequestConfig): void{
+function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
   config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
-
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
-  const {url, params} = config
+  const { url, params } = config
   return buildUrl(url, params)
 }
-function transformRequestData (config: AxiosRequestConfig): any {
+function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
 
-
-function transformHeaders (config: AxiosRequestConfig) {
+function transformHeaders(config: AxiosRequestConfig) {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
