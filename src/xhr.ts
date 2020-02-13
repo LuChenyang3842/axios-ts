649ea6ecabbf08e16eac368d663289ae20feceea
CHENYANGLu <clu3842@gmail.com>
2020-02-13 15:06:43 +0800

    feat: new feature: error handle
    support handle errors including newtwork error, timeout etc. In addition, in the response, we
    provide error message and the request config

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,19 +1,38 @@
 import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from './types'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
+
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
 
     const request = new XMLHttpRequest()
 
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {
+      request.timeout = timeout
+    }
 
     request.open(method.toUpperCase(), url, true)
 
+    request.ontimeout = function handleTimeout() {
+      reject(
+        createError(`Timeout of ${config.timeout} ms exceeded`, config, 'ECONNABORTED', request)
+      )
+    }
+
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request))
+    }
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+
+      if (request.status === 0) {
+        //若出现网络错误|超时，status值为0
         return
       }
 
@@ -28,7 +47,20 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
     }
 
     Object.keys(headers).forEach(name => {
@@ -42,3 +74,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     request.send(data)
   })
 }
+
+// function handleResponse(response: AxiosResponse) {
+//   if (response.status >= 200 && response.status < 300) {
+//     resolve(response)
+//   } else {
+//     reject(new Error(`Request failed with status code ${response.status}`))
+//   }
+// }
